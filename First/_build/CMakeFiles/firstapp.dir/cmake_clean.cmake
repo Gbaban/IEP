9762a5a6362dbcfdcf980129c805018b65c4b359
Gbaban <gabriel.baban@student.upt.ro>
2018-10-17 11:20:23 +0300

    CMakeFile for two projects. One a library and one that uses it

---
+++ First/_build/CMakeFiles/firstapp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/firstapp.dir/first.cpp.o"
+  "firstapp.pdb"
+  "firstapp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/firstapp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Main/main.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include"Library/mylib.h"
+
+int main(int argc, char const *argv[])
+{
+    std::cout << sum(2,8)<<std::endl;
+    return 0;
+}
